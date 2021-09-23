a0089bfedb336027a4d635034754c4767cfc7836
VirajThale <virajthale12@gmail.com>
2021-09-23 04:30:44 +0000

    Calculator programm

--- src/test/Calculator.java
+++ src/test/Calculator.java
@@ -2,6 +2,19 @@ package test;
 
 public class Calculator {
 public static void main (String[] args) {
-	System.out.print("Calculator file");
+	int sum;
+	int num1 =12;
+	int num2 =20;
+	
+	sum= num1+num2;
+	System.out.println("Your sum:"+sum);
+	int sub= num1 - num2;
+	System.out.println("your sub:"+sub);
+	int mul= num1*num2;
+	System.out.println("your mul: "+sum);
+	float div= num1 / num2;
+	System.out.println("your div:"+div);
+			
+	
 }
 }
